da2893d06f43b0560e4c3c597683db64b246e647
Nitheesh Prabu <nitheeshprabu.s@gmail.com>
2020-03-22 00:16:00 +0530

    adding provider pattern

---
+++ shop_app/lib/screens/products_overview_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+import '../widgets/products_grid.dart';
+
+class ProductsOverviewScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('MyShow')),
+      body: ProductsGrid(),
+    );
+  }
+}
---
+++ shop_app/lib/widgets/product_item.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../screens/product_detail_screen.dart';
+import '../providers/product_provider.dart';
+
+class ProductItem extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final product = Provider.of<ProductProvider>(context);
+
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: GridTile(
+        child: GestureDetector(
+          onTap: () {
+            Navigator.of(context).pushNamed(
+              ProductDetailScreen.routeName,
+              arguments: product.id,
+            );
+          },
+          child: Image.network(
+            product.imageUrl,
+            fit: BoxFit.cover,
+          ),
+        ),
+        footer: GridTileBar(
+          leading: IconButton(
+            icon: Icon(
+              product.isFavourite ? Icons.favorite : Icons.favorite_border,
+            ),
+            color: Theme.of(context).accentColor,
+            onPressed: () {
+              product.toggleFavouriteStatus();
+            },
+          ),
+          trailing: IconButton(
+            icon: Icon(Icons.shopping_cart),
+            color: Theme.of(context).accentColor,
+            onPressed: () {},
+          ),
+          backgroundColor: Colors.black87,
+          title: Text(
+            product.title,
+            textAlign: TextAlign.center,
+          ),
+        ),
+      ),
+    );
+  }
+}
