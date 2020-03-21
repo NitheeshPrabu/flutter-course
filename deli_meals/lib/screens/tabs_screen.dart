52c9947ec618f48ccde4e04c5da54934cc45e7f4
Nitheesh Prabu <nitheeshprabu.s@gmail.com>
2020-03-21 21:39:29 +0530

    added drawer

--- deli_meals/lib/screens/tabs_screen.dart
+++ deli_meals/lib/screens/tabs_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import './categories_screen.dart';
 import './favourites_screen.dart';
+import '../widgets/main_drawer.dart';
 
 class TabsScreen extends StatefulWidget {
   @override
@@ -45,7 +46,10 @@ class _TabsScreenState extends State<TabsScreen> {
     //   ),
     // );
     return Scaffold(
-      appBar: AppBar(title: Text(_pages[_selectedPageIndex]['title'])),
+      appBar: AppBar(
+        title: Text(_pages[_selectedPageIndex]['title']),
+      ),
+      drawer: MainDrawer(),
       body: _pages[_selectedPageIndex]['page'],
       bottomNavigationBar: BottomNavigationBar(
           onTap: _selectPage,
