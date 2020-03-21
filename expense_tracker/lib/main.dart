c17cf15d5129bb4c0678e4521ab23137357a77b4
Nitheesh Prabu <nitheeshprabu.s@gmail.com>
2020-03-21 13:27:46 +0530

    added theming

--- expense_tracker/lib/main.dart
+++ expense_tracker/lib/main.dart
@@ -10,7 +10,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter App',
+      title: 'Expense Tracker',
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+        accentColor: Colors.amber,
+      ),
       home: MyHomePage(),
     );
   }
@@ -70,7 +74,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Flutter App'),
+        title: Text('Expense Tracker'),
         actions: <Widget>[
           IconButton(
             icon: Icon(Icons.add),
@@ -85,7 +89,6 @@ class _MyHomePageState extends State<MyHomePage> {
             Container(
               width: double.infinity,
               child: Card(
-                color: Colors.blue,
                 child: Text('CHART!'),
                 elevation: 5,
               ),
