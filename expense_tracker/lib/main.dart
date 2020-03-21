ff963195fa6955ab008ced9d8b4080b7bcecbeab
Nitheesh Prabu <nitheeshprabu.s@gmail.com>
2020-03-21 15:24:13 +0530

    delete transactions

--- expense_tracker/lib/main.dart
+++ expense_tracker/lib/main.dart
@@ -70,6 +70,12 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+  void _deleteTransaction(String id) {
+    setState(() {
+      _userTransactions.removeWhere((transaction) => transaction.id == id);
+    });
+  }
+
   void _startAddNewTransaction(BuildContext ctx) {
     showModalBottomSheet(
       context: ctx,
@@ -105,7 +111,9 @@ class _MyHomePageState extends State<MyHomePage> {
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
             Chart(_recentTransactions),
-            TransactionList(_userTransactions)
+            Expanded(
+              child: TransactionList(_userTransactions, _deleteTransaction),
+            )
           ],
         ),
       ),
