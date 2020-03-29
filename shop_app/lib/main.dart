d087ea20f21b8b9cfb7381767ff9a11d078b82f4
Nitheesh Prabu <nitheeshprabu.s@gmail.com>
2020-03-29 10:51:35 +0530

    added animations

--- shop_app/lib/main.dart
+++ shop_app/lib/main.dart
@@ -13,6 +13,7 @@ import './providers/products_provider.dart';
 import './providers/cart_provider.dart';
 import './providers/orders_provider.dart';
 import './providers/auth_provider.dart';
+import './helpers/custom_route.dart';
 
 void main() => runApp(MyApp());
 
@@ -53,6 +54,12 @@ class MyApp extends StatelessWidget {
                 primarySwatch: Colors.purple,
                 accentColor: Colors.deepOrange,
                 fontFamily: 'Lato',
+                pageTransitionsTheme: PageTransitionsTheme(
+                  builders: {
+                    TargetPlatform.android: CustomPageTransitionBuilder(),
+                    TargetPlatform.iOS: CustomPageTransitionBuilder(),
+                  },
+                ),
               ),
               home: authData.isAuth
                   ? ProductsOverviewScreen()
