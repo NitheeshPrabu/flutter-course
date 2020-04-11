6f074d604653497ff4ba0642a6c0670c012fd6d2
Nitheesh Prabu <nitheeshprabu.s@gmail.com>
2020-04-11 19:42:49 +0530

    adding bloc

--- news/lib/src/app.dart
+++ news/lib/src/app.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/material.dart';
 
+import './blocs/stories_provider.dart';
+import './screens/news_list.dart';
+
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return StoriesProvider(
+      child: MaterialApp(
+        title: 'News!',
+        home: NewsList(),
+      ),
+    );
   }
 }
---
+++ news/lib/src/screens/news_list.dart
@@ -0,0 +1,38 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+import '../blocs/stories_provider.dart';
+
+class NewsList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final bloc = StoriesProvider.of(context);
+    bloc.fetchTopIds();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Top News'),
+      ),
+      body: buildList(bloc),
+    );
+  }
+
+  Widget buildList(StoriesBloc bloc) {
+    return StreamBuilder(
+      builder: (context, AsyncSnapshot<List<int>> snapshot) {
+        if (!snapshot.hasData) {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+        return ListView.builder(
+          itemBuilder: (ctx, index) {
+            return Text('${snapshot.data[index]}');
+          },
+          itemCount: snapshot.data.length,
+        );
+      },
+      stream: bloc.topIds,
+    );
+  }
+}
