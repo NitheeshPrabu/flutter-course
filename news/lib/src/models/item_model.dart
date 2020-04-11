4899a967f1a439da5e971b75c8985af90134aa8d
Nitheesh Prabu <nitheeshprabu.s@gmail.com>
2020-04-11 18:13:31 +0530

    repository

--- news/lib/src/models/item_model.dart
+++ news/lib/src/models/item_model.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 class ItemModel {
   final int id;
   final bool deleted;
@@ -27,4 +29,37 @@ class ItemModel {
         score = parsedJson['score'],
         title = parsedJson['title'],
         descendants = parsedJson['descendants'];
+
+  ItemModel.fromDb(Map<String, dynamic> parsedRecord)
+      : id = parsedRecord['id'],
+        deleted = parsedRecord['deleted'] == 1,
+        type = parsedRecord['type'],
+        by = parsedRecord['by'],
+        time = parsedRecord['time'],
+        text = parsedRecord['text'],
+        dead = parsedRecord['dead'] == 1,
+        parent = parsedRecord['parent'],
+        kids = jsonDecode(parsedRecord['kids']),
+        url = parsedRecord['url'],
+        score = parsedRecord['score'],
+        title = parsedRecord['title'],
+        descendants = parsedRecord['descendants'];
+
+  Map<String, dynamic> toMapForDb() {
+    return <String, dynamic>{
+      'id': id,
+      'type': type,
+      'by': by,
+      'time': time,
+      'text': text,
+      'parent': parent,
+      'url': url,
+      'score': score,
+      'title': title,
+      'descendants': descendants,
+      'deleted': deleted ? 1 : 0,
+      'dead': dead ? 1 : 0,
+      'kids': jsonEncode(kids),
+    };
+  }
 }
